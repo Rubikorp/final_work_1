6b2c168d2749ac58c9d9b47ebae307b6260cf592
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-11-08 22:32:08 +0500

    Написал код считывания данных пользователя в массив, ввод длины массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] arr)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+// Считывание данных пользователя
+Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+string[] arr = new string[length];
+
+for (int i = 0; i < length; i++)
+{
+    Console.Write($"Ведите слово {i}:");
+    string text = Console.ReadLine();
+    arr[i] = text;
+}
+PrintArray(arr);
+
